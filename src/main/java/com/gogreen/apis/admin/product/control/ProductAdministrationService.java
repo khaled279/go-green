56039fc9895e384e3ec06377bf025f16fb96f7f7
khaled279 <khaled.sabed30@yahoo.com>
2023-04-21 13:43:14 +0200

    fixed product category

--- src/main/java/com/gogreen/apis/admin/product/control/ProductAdministrationService.java
+++ src/main/java/com/gogreen/apis/admin/product/control/ProductAdministrationService.java
@@ -2,6 +2,7 @@ package com.gogreen.apis.admin.product.control;
 
 import com.gogreen.apis.admin.vendor.main.repository.VendorRepository;
 import com.gogreen.apis.admin.vendor.user.repository.VendorUserRepository;
+import com.gogreen.apis.lookups.repository.ProductCategoryRepository;
 import com.gogreen.apis.product.boundaries.helper.ProductMapper;
 import com.gogreen.apis.product.control.ProductService;
 import com.gogreen.apis.product.repositories.ProductRepository;
@@ -10,6 +11,7 @@ import com.gogreen.models.auth.dtos.UserDetailsImpl;
 import com.gogreen.models.auth.enums.UserTypeEnum;
 import com.gogreen.models.product.dtos.ProductDto;
 import com.gogreen.models.product.dtos.ProductSearchDto;
+import com.gogreen.models.product.entities.ProductCategoryEntity;
 import com.gogreen.models.product.entities.ProductEntity;
 import com.gogreen.models.user.entities.VendorUserEntity;
 import com.gogreen.models.vendor.entities.VendorEntity;
@@ -32,6 +34,8 @@ public class ProductAdministrationService {
 	private final VendorRepository vendorRepository;
 
 	private final VendorUserRepository vendorUserRepository;
+
+	private final ProductCategoryRepository productCategoryRepository;
 
 	public Page<ProductDto> listProducts(Long vendorId, Pageable pageable) {
 		ProductSearchDto productSearchDto = new ProductSearchDto();
@@ -57,6 +61,11 @@ public class ProductAdministrationService {
 				.orElseThrow(() -> new SystemException(HttpStatus.FORBIDDEN,
 						"vendor is inActive"));
 		productEntity.setVendor(vendor);
+		ProductCategoryEntity productCategory = this.productCategoryRepository.findById(
+				productDto.getProductCategoryId()).orElseThrow(
+				() -> new SystemException(HttpStatus.BAD_REQUEST,
+						"invalid product category Id"));
+		productEntity.setProductCategory(productCategory);
 		productEntity = this.productRepository.saveAndFlush(productEntity);
 		Set<ProductEntity> productEntities = vendor.getProducts();
 		productEntities.add(productEntity);
